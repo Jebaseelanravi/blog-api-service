6fd392ee4fa799ed15cdfcc2d2fac97a743b5bc0
Jebaseelanravi <jebaseelanravi96@gmail.com>
2021-02-07 23:03:24 +0530

    heroku deploy - added static dir

--- blogapi/settings.py
+++ blogapi/settings.py
@@ -128,4 +128,6 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'graphene_django/static'),
+)
